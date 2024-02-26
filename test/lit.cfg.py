ec28ffa99d29a33ba45b79afc7c4ad2252b0b630
Djordje Todorovic <djolertrk@gmail.com>
2024-02-26 11:24:41 +0100

    Introduce llvm build and start porting tests lit

---
+++ test/lit.cfg.py
@@ -0,0 +1,43 @@
+# -*- Python -*-
+
+import os
+
+import lit.formats
+import lit.util
+
+from lit.llvm import llvm_config
+
+# Configuration file for the 'lit' test runner.
+
+# name: The name of this test suite.
+config.name = 'binder'
+
+# test_format: The test format to use to interpret tests.
+config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
+
+# suffixes: A list of file extensions to treat as test files.
+config.suffixes = ['.test']
+
+# excludes: A list of directories to exclude from the testsuite.
+config.excludes = ['CMakeLists.txt']
+
+# test_source_root: The root path where tests are located.
+config.test_source_root = os.path.dirname(__file__)
+
+# test_exec_root: The root path where tests should be run.
+config.test_exec_root = os.path.join(config.binder_build_dir, 'test')
+
+# Configure environment variables.
+config.substitutions.append(('%PATH%', config.environment['PATH']))
+llvm_config.with_system_environment(['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP'])
+
+# Tweak the PATH to include the tools dir.
+llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
+
+# Register substitutions.
+llvm_config.use_default_substitutions()
+llvm_config.use_clang()
+llvm_config.add_tool_substitutions(
+    ['binder'],
+    [config.binder_bin_dir]
+)
